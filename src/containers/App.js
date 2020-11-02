16e2150f4ce09d6c83f06e00a278e02907683c6e
Nicholas Gibson <nckgibson4@gmail.com>
2020-11-02 10:57:42 -0800

    Initial

---
+++ src/containers/App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import './App.css';
+import Layout from '../components/Layout/Layout';
+import Home from '../components/Home/Home';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import NotFound from '../components/ErrorPages/NotFound/NotFound';
+// import OwnerList from './Owner/OwnerList/OwnerList';
+import asyncComponent from '../hoc/AsyncComponent/AsyncComponent';
+import InternalServer from '../components/ErrorPages/InternalServer/InternalServer';
+
+const AsyncOwnerList = asyncComponent(() => {
+  return import('./Owner/OwnerList/OwnerList');
+});
+const AsyncOwnerDetails = asyncComponent(() => {
+  return import('./Owner/OwnerDetails/OwnerDetails');
+});
+const AsyncCreateOwner = asyncComponent(() => {
+  return import('./Owner/CreateOwner/CreateOwner');
+});
+const AsyncUpdateOwner = asyncComponent(() => {
+  return import('./Owner/UpdateOwner/UpdateOwner');
+});
+const AsyncDeleteOwner = asyncComponent(() => {
+  return import('./Owner/DeleteOwner/DeleteOwner');
+});
+function App() {
+  return (
+    <BrowserRouter>
+
+    <Layout>
+      <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/owner-list" component={AsyncOwnerList} />
+      <Route path="/ownerDetails/:id" component={AsyncOwnerDetails} />
+      <Route path="/createOwner" component={AsyncCreateOwner} />
+      <Route path="/updateOwner/:id" component={AsyncUpdateOwner} />
+      <Route path="/deleteOwner/:id" component={AsyncDeleteOwner} />
+      <Route path="/500" component={InternalServer} />
+      <Route path="*" component={NotFound} />
+
+      </Switch>
+
+    </Layout>
+
+    </BrowserRouter>
+
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
+import repositoryReducer from './store/reducers/repositoryReducers';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import errorHandlerReducer from './store/reducers/errorHandlerReducer';
+
+const rootReducers = combineReducers({
+  repository: repositoryReducer,
+  errorHandler: errorHandlerReducer
+})
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(
+  rootReducers,
+  composeEnhancers(
+      applyMiddleware(thunk)
+  )
+);
 
 ReactDOM.render(
+  <Provider store={store}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
